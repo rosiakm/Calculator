7c73696c991c740fb8171cfa4e9d6c4f96b3e3b9
Mateusz Rosiak <mateusz.rosiak89@gmail.com>
2025-10-17 09:41:39 +0200

    calculator project

--- src/main/java/models/Calculator.java
+++ src/main/java/models/Calculator.java
@@ -1,5 +1,32 @@
 package models;
 
-public class Calculator {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Calculator {
+    Logger logger = LoggerFactory.getLogger(Calculator.class);
+
+    public Double add(double a, double b){
+        logger.info("I am adding: {} and {}", a, b);
+        return a + b;
+    }
+
+    public Double subtract(double a, double b){
+        logger.info("I am subtracting: {} and {}", a, b);
+        return  a - b;
+    }
+
+    public Double multiply(double a, double b){
+        logger.info("I am multiplying: {} and {}", a, b);
+        return a * b;
+    }
+
+    public Double divide(double a, double b){
+        if(b == 0){
+            logger.info("The arithmetic exception has been thrown");
+            throw new ArithmeticException("The divisor equals 0");
+        }
+        logger.info("I am dividing: {} and {}", a, b);
+        return a/b;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,32 @@
-public class CalculatorTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest extends BaseTest {
+
+    @Test
+    public void addTest(){
+        assertEquals(5, calculator.add(3,2));
+    }
+
+    @Test
+    public void subtractTest(){
+        assertEquals(5,calculator.subtract(8,3));
+    }
+
+    @Test
+    public void multiplyTest(){
+        assertEquals(25, calculator.multiply(5,5));
+    }
+
+    @Test
+    public void divideTest(){
+        assertEquals(10, calculator.divide(100,10));
+    }
+
+    @Test
+    public void divideByZeroTest(){
+        assertThrows(ArithmeticException.class, () -> calculator.divide(10,0));
+    }
 }
